aae3880a6309a2df00c19cdf9f348ef2b2832424
Agustin Martinez <agustin.martinez@sonaype.com>
2023-02-21 09:57:08 -0800

    pr

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ def to_seconds(milliseconds: List[float]) -> List[int]:
 
 my_list: List[int] = [1]
 my_list = to_seconds(my_list) # Pyre errors here!
+
+
+def zeroes(number_of_elements: int) -> List[float]:
+    a = [0] * number_of_elements
+    return a # Pyre errors here!
