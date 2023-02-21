321c8aa91197f108ef85e8f5586e6dbe37004616
Agustin Martinez <agustin.martinez@sonaype.com>
2023-02-21 09:55:07 -0800

    main branch

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-def to_seconds(milliseconds: List[float]) -> List[int]:
-    return [int(x/1000.0) for x in milliseconds]
-
-my_list: List[int] = [1]
-my_list = to_seconds(my_list) # Pyre errors here!
+print("main")
