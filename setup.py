123891fb7ff67d34e1b23c239fca248098b457d0
Agustin Martinez <agustin.martinez@sonaype.com>
2023-03-21 16:18:03 -0700

    import emoji

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pythontesting',
+      version='0.1',
+      description='The best testing ever',
+      author='me',
+      license='MIT',
+      install_requires=[
+          'emoji',
+      ])
